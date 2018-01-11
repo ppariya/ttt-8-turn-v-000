f1c0bc3081fd58f4f692f19bea782e704a0e522c
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-11 01:10:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def turn(board)
     until valid_move?(board, index) do
         puts "Please enter 1-9:"
         input = gets.strip
-        
+        index = input_to_index(input)
     end
   end
 
