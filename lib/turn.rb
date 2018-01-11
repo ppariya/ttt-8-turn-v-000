0cd0715c27930129e7f842d6546b993731ffe419
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-11 00:54:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def input_to_index(input)
+  input.to_i - 1
+end
 
 def valid_move?(board, index)
   index.between?(0, 8) && !(position_taken?(board, index))
